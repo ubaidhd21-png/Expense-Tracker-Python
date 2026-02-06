321b248581e1094d18d6b83d7a44099a9dc03ecc
ubaidhd21-png <ubaidhd21@gmail.com>
2026-02-06 23:11:54 +0530

    Update expense_manager.py

--- expense_manager.py
+++ expense_manager.py
@@ -1,1 +1,13 @@
+from file_handler import load_expenses, save_expenses
 
+def add_expense(amount, category, description):
+    expenses = load_expenses()
+
+    expense = {
+        "amount": amount,
+        "category": category,
+        "description": description
+    }
+
+    expenses.append(expense)
+    save_expenses(expenses)
