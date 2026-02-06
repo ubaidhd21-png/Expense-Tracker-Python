804a68d8673c0681e5ef64ca84f87f1ca2b2f108
ubaidhd21-png <ubaidhd21@gmail.com>
2026-02-06 23:11:10 +0530

    Update File_handler.py

--- File_handler.py
+++ File_handler.py
@@ -1,1 +1,14 @@
+import json
+import os
 
+FILE_NAME = "expenses.json"
+
+def load_expenses():
+    if not os.path.exists(FILE_NAME):
+        return []
+    with open(FILE_NAME, "r") as file:
+        return json.load(file)
+
+def save_expenses(expenses):
+    with open(FILE_NAME, "w") as file:
+        json.dump(expenses, file, indent=4)
