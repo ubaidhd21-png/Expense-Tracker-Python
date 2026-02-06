eddced9e8b93909507a67ebf619736f56dbed3cb
ubaidhd21-png <ubaidhd21@gmail.com>
2026-02-06 23:12:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-def main ():
-  print("Expense Tracker started") 
+
+
+def main():
+    print("Expense Tracker")
+    amount = float(input("Enter amount: "))
+    category = input("Enter category: ")
+    description = input("Enter description: ")
+
+    add_expense(amount, category, description)
+    print("Expense added successfully")
 
 if __name__ == "__main__":
-  main() 
+    main()
