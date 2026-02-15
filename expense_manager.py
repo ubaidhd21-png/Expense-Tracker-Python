831f025b8e16030181ec8ce2c6f51a0dce6e586b
ubaidhd21-png <ubaidhd21@gmail.com>
2026-02-15 20:08:32 +0530

    Update expense_manager.py

--- expense_manager.py
+++ expense_manager.py
@@ -1,5 +1,6 @@
 from file_handler import load_expenses, save_expenses
 
+
 def add_expense(amount, category, description):
     expenses = load_expenses()
 
@@ -11,3 +12,13 @@ def add_expense(amount, category, description):
 
     expenses.append(expense)
     save_expenses(expenses)
+
+
+def calculate_total():
+    expenses = load_expenses()
+    total = 0
+
+    for expense in expenses:
+        total += expense["amount"]
+
+    return total
