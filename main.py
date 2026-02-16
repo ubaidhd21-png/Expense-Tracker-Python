037f87c3f11345023bcf1c0609fd23b624942531
ubaidhd21-png <ubaidhd21@gmail.com>
2026-02-16 21:54:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
-from expense_manager import add_expense, calculate_total
+from expense_manager import add_expense, calculate_total, list_expenses
+
 
 def main():
     print("Expense Tracker")
+
     amount = float(input("Enter amount: "))
     category = input("Enter category: ")
     description = input("Enter description: ")
 
     add_expense(amount, category, description)
-    print("Expense added successfully")
 
+    print("Expense added successfully")
     print("Total spent:", calculate_total())
+
+    print("\nAll Expenses:")
+    list_expenses()
+
 
 if __name__ == "__main__":
     main()
