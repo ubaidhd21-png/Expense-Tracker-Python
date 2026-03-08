33ce26537ec7a00f62e9befc40964143cbb48e99
ubaidhd21-png <ubaidhd21@gmail.com>
2026-03-08 19:14:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,41 @@
-from expense_manager import add_expense, calculate_total, list_expenses
+from expense_manager import add_expense, calculate_total, list_expenses, filter_by_category
 
 
 def main():
-    print("Expense Tracker")
-    print("1. Add expense")
-    print("2. Show total")
-    print("3. List expenses")
+    while True:
+        print("\nExpense Tracker")
+        print("1. Add expense")
+        print("2. Show total")
+        print("3. List expenses")
+        print("4. Filter by category")
+        print("5. Exit")
 
-    choice = input("Choose option: ")
+        choice = input("Choose option: ")
 
-    if choice == "1":
-        amount = float(input("Enter amount: "))
-        category = input("Enter category: ")
-        description = input("Enter description: ")
-        add_expense(amount, category, description)
-        print("Expense added!")
+        if choice == "1":
+            amount = float(input("Enter amount: "))
+            category = input("Enter category: ")
+            description = input("Enter description: ")
 
-    elif choice == "2":
-        print("Total spent:", calculate_total())
+            add_expense(amount, category, description)
+            print("Expense added!")
 
-    elif choice == "3":
-        list_expenses()
+        elif choice == "2":
+            print("Total spent:", calculate_total())
 
-    else:
-        print("Invalid choice")
+        elif choice == "3":
+            list_expenses()
+
+        elif choice == "4":
+            category = input("Enter category to filter: ")
+            filter_by_category(category)
+
+        elif choice == "5":
+            print("Goodbye!")
+            break
+
+        else:
+            print("Invalid choice")
 
 
 if __name__ == "__main__":
